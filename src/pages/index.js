d8d57a9d044e80f7884d72079891b1caf5982fc2
OOXXXX <zhupx2003@126.com>
2020-07-04 16:34:30 +0800

    Hello world(First discover)

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,16 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Hello world</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </div>
+
   </div>
 )
 
